9f59e5028ff5cb690c4326f99d8fa38bb279328e
Hu Chengli <huchengli95@gmail.com>
2026-03-29 16:11:57 +0800

    Enhance expense loading with error handling
    Refactor load_expenses to handle empty lines and unknown categories, and improve error handling.

--- core/file_handler.py
+++ core/file_handler.py
@@ -1,19 +1,50 @@
 import json
-from data_model.Schemas import Expense, ExpenseCategory
 from datetime import datetime
+from data_model.Schemas import Expense, ExpenseCategory
 
 def load_expenses(filepath):
-    expenses = []
-    with open(filepath, 'r') as file:
-        for line in file:
-            data = json.loads(line.strip())
-            date_obj = datetime.strptime(data['date'], "%Y-%m-%d").date()            
-            expense = Expense(
-                id=data['id'],
-                date=date_obj,
-                amount=data['amount'],
-                category=ExpenseCategory[data['category'].upper()],
-                description=data['description']
-            )
-            expenses.append(expense)
+    expenses =[]
+    try:
+        with open(filepath, 'r') as file:
+            for line in file:
+                line = line.strip()
+                if line == "":
+                    continue                   
+                try:
+                    data = json.loads(line)
+                    date_obj = datetime.strptime(data['date'], "%Y-%m-%d").date()                    
+                    raw_category = data['category'].upper()
+                    try:
+                        category_enum = ExpenseCategory[raw_category]
+                    except:
+                        category_enum = ExpenseCategory.UNCATEGORIZED
+                    expense = Expense(
+                        id=data['id'],
+                        date=date_obj,
+                        amount=float(data['amount']),
+                        category=category_enum,
+                        description=data['description']
+                    )
+                    expenses.append(expense)                    
+                except:
+                    print("Error reading a line in the file.")                    
+    except FileNotFoundError:
+        print("File not found. Starting with an empty list.")            
+    print("Successfully loaded", len(expenses), "expenses.")
     return expenses
+
+def save_expenses(filepath, expenses):
+    try:
+        with open(filepath, 'w') as file:
+            for exp in expenses:
+                exp_dict = {
+                    "id": exp.id,
+                    "date": str(exp.date),
+                    "amount": float(exp.amount),
+                    "category": exp.category.name.lower(),
+                    "description": exp.description
+                }                
+                file.write(json.dumps(exp_dict) + "\n")                
+        print("Saved", len(expenses), "expenses.")        
+    except:
+        print("Error saving file.")
