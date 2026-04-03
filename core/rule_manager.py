a9441e465a98a3407ae81ca87862417d301c79fa
. <107795824+aaqwesas@users.noreply.github.com>
2026-04-03 19:06:43 +0800

    naming

--- core/rule_manager.py
+++ core/rule_manager.py
@@ -1,11 +1,14 @@
 from data_model.rules import BudgetRule
 from data_model.schemas import Transaction
 
-class AlertManager:
+
+class RuleManager:
     def __init__(self):
         self.rules: list[BudgetRule] = []
+
     def add_rule(self, rule: BudgetRule):
         self.rules.append(rule)
+
     def process_transaction(self, transaction: Transaction):
         for rule in self.rules:
             rule.evaluate(transaction)
