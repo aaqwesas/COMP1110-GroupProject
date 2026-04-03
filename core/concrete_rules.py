aa0a5d69f440d6a45f67c58ce4879bf151b3de99
. <107795824+aaqwesas@users.noreply.github.com>
2026-04-03 18:36:28 +0800

    total spending should be put somewhere else for separating the concern (rule shoulnd't be responsible for storing the total spending)

--- core/concrete_rules.py
+++ core/concrete_rules.py
@@ -1,18 +1,11 @@
-from collections.abc import Callable
 from dataclasses import dataclass
 import operator
-from datetime import date
-from typing import Any
 from data_model.rules import BudgetRule
 from data_model.schemas import Transaction, Expense
-from data_model.categories import Categories
-from data_model.alerts import Alert
 
 
 @dataclass(slots=True)
 class CategoryBudgetRule(BudgetRule):
-    current_spending: float = 0.0
-
     def evaluate(self, transaction: Transaction) -> bool:
         if not isinstance(transaction, Expense):
             return False
@@ -20,11 +13,10 @@ class CategoryBudgetRule(BudgetRule):
         if transaction.category != self.schema.category:
             return False
 
-        self.current_spending += transaction.amount
-        if self.operator(self.current_spending, self.threshold):
+        if self.operator(transaction.amount, self.threshold):
             self.alert(
                 message=f"You have exceeded your {self.schema.category.name} budget! "
-                f"Spent: {self.current_spending}, Limit: {self.threshold}"
+                f"Spent: {transaction.amount}, Limit: {self.threshold}"
             )
             return True
         return False
