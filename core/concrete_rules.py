19416e87d9f5f6282cba0dee673fc650ce3d9968
Hu Chengli <huchengli95@gmail.com>
2026-04-10 14:38:13 +0800

    Rewrite CategoryBudgetRule to sum only the recent transactions (within the period) that match the specified category

--- core/concrete_rules.py
+++ core/concrete_rules.py
@@ -6,17 +6,28 @@ from data_model.schemas import Transaction, Expense
 
 @dataclass(slots=True)
 class CategoryBudgetRule(BudgetRule):
-    def evaluate(self, transaction: Transaction) -> bool:
+    def evaluate(self, transaction: Transaction, history: list[Transaction]) -> bool:
         if not isinstance(transaction, Expense):
             return False
 
         if transaction.category != self.schema.category:
             return False
 
-        if self.operator(transaction.amount, self.threshold):
+        start_date, end_date = self.get_range(transaction.date)
+
+        period_total = 0.0
+        for transaction in history:
+            if isinstance(transaction, Expense) and transaction.category == self.schema.category:
+                if start_date <= transaction.date <= end_date:
+                    period_total += transaction.amount
+
+        total_with_current = period_total + transaction.amount
+
+        if self.operator(total_with_current, self.threshold):
             self.alert(
-                message=f"You have exceeded your {self.schema.category.name} budget! "
-                f"Spent: {transaction.amount}, Limit: {self.threshold}"
+                message = f"You have exceeded your {self.schema.category.name} budget from the last {self.period} days! "
+                f"Limit: {self.threshold:.2f}, Acumulated: {total_with_current:.2f}"
+                f"(includes the new ransaction of ${transaction.amount:.2f})"
             )
             return True
         return False
