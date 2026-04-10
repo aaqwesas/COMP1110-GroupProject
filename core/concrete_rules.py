aca621237a8b2f42a72e9b3faadaf941d92ce83c
Hu Chengli <huchengli95@gmail.com>
2026-04-10 18:37:25 +0800

    Refactor CategoryBudgetRule evaluation logic and add SingleTransactionRule for large transaction alerts

--- core/concrete_rules.py
+++ core/concrete_rules.py
@@ -2,21 +2,17 @@ from dataclasses import dataclass
 from data_model.rules import BudgetRule
 from data_model.schemas import Transaction, Expense
 
-
 @dataclass(slots=True)
 class CategoryBudgetRule(BudgetRule):
     def evaluate(self, transaction: Transaction, history: list[Transaction]) -> bool:
         if not isinstance(transaction, Expense):
             return False
 
-        if transaction.category != self.schema.category:
-            return False
-
         start_date, end_date = self.get_range(transaction.date)
-
         period_total = 0.0
+
         for transaction in history:
-            if isinstance(transaction, Expense) and transaction.category == self.schema.category:
+            if isinstance(transaction, Expense):
                 if start_date <= transaction.date <= end_date:
                     period_total += transaction.amount
 
@@ -24,9 +20,25 @@ class CategoryBudgetRule(BudgetRule):
 
         if self.operator(total_with_current, self.threshold):
             self.alert(
-                message = f"You have exceeded your {self.schema.category.name} budget from the last {self.period} days!"
+                message = f"You have exceeded your overall budget from the last {self.period} days!"
                 f"Limit: {self.threshold:.2f}, Accumulated: {total_with_current:.2f}"
                 f"(includes the new transaction of ${transaction.amount:.2f})"
+            )
+            return True
+        return False
+
+@dataclass(slots=True)
+class SingleTransactionRule(BudgetRule):
+    def evaluate(self, transaction: Transaction, history: list[Transaction]) -> bool:
+
+        if not isinstance(transaction, Expense):
+            return False
+
+        if self.operator(transaction.amount, self.threshold):
+            self.alert(
+                message = f"Large transaction alert! A single expense of ${transaction.amount:.2f}"
+                          f"exceeded the limit of {self.threshold:.2f}"
+                          f"(Description: {transaction.description})."
             )
             return True
         return False
