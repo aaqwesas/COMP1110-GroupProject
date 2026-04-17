b651d4cb7ce588c150a12c61c38edb7079417b0e
Hu Chengli <huchengli95@gmail.com>
2026-04-17 11:58:46 +0800

    Optimize RuleManager by pre-filtering transaction history based on the maximum required rule period

--- core/rule_manager.py
+++ core/rule_manager.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from data_model.rules import BudgetRule
 from data_model.schemas import Transaction
 
@@ -9,5 +10,18 @@ class RuleManager:
         self.rules.append(rule)
 
     def process_transaction(self, transaction: Transaction, history: list[Transaction]):
+        periods = [rule.period for rule in self.rules if rule.period is not None]
+
+        if periods:
+            max_period = max(periods)
+            cutoff_date = transaction.date - timedelta(days=max_period)
+
+            recent_history = [
+                t for t in history
+                if cutoff_date <= t.date <= transaction.date
+            ]
+        else:
+            recent_history = []
+
         for rule in self.rules:
-            rule.evaluate(transaction = transaction, history = history)
+            rule.evaluate(transaction=transaction, history=recent_history)
